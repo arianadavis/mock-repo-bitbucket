69b96f5d9723c2c999c99da731a1250495b49f4c
Ariana Davis <adavis@warriors.com>
2019-04-19 22:22:41 +0000

    add code in files types: .ts remove code in files types: .ts

--- Contributions-Importer-For-Github/run_script.py
+++ Contributions-Importer-For-Github/run_script.py
@@ -5,10 +5,12 @@ from git_contributions_importer import *
 
 # Your private repo or Bitbucket repo
 repo = git.Repo("/Users/adavis/Desktop/dev/chasecenter-com-ng")
+repoTwo = git.Repo("/Users/adavis/Desktop/dev/chasecenter-com-api")
 
 # Your mock repo
 mock_repo = git.Repo("/Users/adavis/Desktop/dev/mock-repo-bitbucket")
-importer = Importer([repo], mock_repo)
+
+importer = Importer([repo, repoTwo], mock_repo)
 
 # I use both my personal email and work email here,
 # Since the private repo uses work email, and Github profiles uses
